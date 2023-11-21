5684d67a2a35ef0984c3d6d3a198a95611ad482c
NatsukiHayashi <su811sh1@gmail.com>
2023-11-21 22:57:25 +0900

    ✨ チャプター34

---
+++ src/components/Payment.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="container">
+        <h1>Payment</h1>
+        <div class="payment">
+            <label>{{ ItemName1 }}</label>
+            <label>{{ ItemPrice1 }} yen</label>
+            <a v-bind:href="url1">bought at...</a>
+        </div>
+        <div class="payment">
+            <label>{{ ItemName2 }}</label>
+            <label>{{ ItemPrice2 }} yen</label>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+const ItemName1 = 'Desk';
+const ItemName2 = 'Bike';
+
+const ItemPrice1 = '40000';
+const ItemPrice2 = '20000';
+
+const url1 = 'https://www.amazon.co.jp/Jiandi-%E3%82%B2%E3%83%BC%E3%83%9F%E3%83%B3%E3%82%B0%E3%83%87%E3%82%B9%E3%82%AF-%E3%83%91%E3%82%BD%E3%82%B3%E3%83%B3%E3%83%87%E3%82%B9%E3%82%AF-%E3%83%A9%E3%83%83%E3%82%AF%E4%BB%98%E3%81%8D%E3%83%87%E3%82%B9%E3%82%AF-%E3%82%AA%E3%83%95%E3%82%A3%E3%82%B9%E3%83%87%E3%82%B9%E3%82%AF/dp/B0B3Y9KXCS/ref=sr_1_5?__mk_ja_JP=%E3%82%AB%E3%82%BF%E3%82%AB%E3%83%8A&crid=9XSA60HQD2J2&keywords=desk&qid=1700574894&sprefix=desk,aps,172&sr=8-5&th=1'
+</script>
+
+<style scoped>
+.container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+.payment {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 80px;
+    width: 400px;
+    background-color: blue;
+    margin-bottom: 8px;
+}
+
+label {
+    font-size: 20px;
+    font-weight: bold;
+}
+</style>
