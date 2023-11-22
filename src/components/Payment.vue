f8f28d398de1b1415fad8f6b5bfb937a36b83212
NatsukiHayashi <su811sh1@gmail.com>
2023-11-22 09:27:46 +0900

    ✨ チャプター37

--- src/components/Payment.vue
+++ src/components/Payment.vue
@@ -1,26 +1,74 @@
 <template>
     <div class="container">
         <h1>Payment</h1>
+        <input v-on:input="inputName1" />
+        <input v-on:input="inputPrice1" />
+        <input v-on:input="inputName2" />
+        <input v-on:input="inputPrice2" />
         <div class="payment">
-            <label>{{ ItemName1 }}</label>
-            <label>{{ ItemPrice1 }} yen</label>
+            <label>{{ Item1.name }}</label>
+            <label>{{ Item1.price }} yen</label>
             <a v-bind:href="url1">bought at...</a>
+            <button v-on:click="buy(Item1.name)">BUY</button>
         </div>
         <div class="payment">
-            <label>{{ ItemName2 }}</label>
-            <label>{{ ItemPrice2 }} yen</label>
+            <label>{{ Item2.name }}</label>
+            <label>{{ Item2.price }} yen</label>
+            <a v-bind:href="url1">bought at...</a>
+            <button v-on:click="buy(Item2.name)">BUY</button>
         </div>
     </div>
 </template>
 
 <script setup lang="ts">
-const ItemName1 = 'Desk';
+import { ref, reactive } from 'vue'
+
+const Item1 = reactive({
+    name: null,
+    price: null,
+});
+
+const Item2 = reactive({
+    name: null,
+    price: null,
+});
+
+// const ItemName1 = ref<string>('Desk');
 const ItemName2 = 'Bike';
 
-const ItemPrice1 = '40000';
+// const ItemPrice1 = '40000';
 const ItemPrice2 = '20000';
 
 const url1 = 'https://www.amazon.co.jp/Jiandi-%E3%82%B2%E3%83%BC%E3%83%9F%E3%83%B3%E3%82%B0%E3%83%87%E3%82%B9%E3%82%AF-%E3%83%91%E3%82%BD%E3%82%B3%E3%83%B3%E3%83%87%E3%82%B9%E3%82%AF-%E3%83%A9%E3%83%83%E3%82%AF%E4%BB%98%E3%81%8D%E3%83%87%E3%82%B9%E3%82%AF-%E3%82%AA%E3%83%95%E3%82%A3%E3%82%B9%E3%83%87%E3%82%B9%E3%82%AF/dp/B0B3Y9KXCS/ref=sr_1_5?__mk_ja_JP=%E3%82%AB%E3%82%BF%E3%82%AB%E3%83%8A&crid=9XSA60HQD2J2&keywords=desk&qid=1700574894&sprefix=desk,aps,172&sr=8-5&th=1'
+
+const buy = (itemName: string) => {
+    alert('Are you sure to buy ' + itemName + '?');
+}
+
+// const input = (event: any) => {
+//     console.log('event:', event.target.value);
+//     ItemName1.value = event.target.value;
+// }
+
+const inputName1 = (event: any) => {
+    console.log('event:', event.target.value);
+    Item1.name = event.target.value;
+}
+
+const inputPrice1 = (event: any) => {
+    console.log('event:', event.target.value);
+    Item1.price = event.target.value;
+}
+
+const inputName2 = (event: any) => {
+    console.log('event:', event.target.value);
+    Item2.name = event.target.value;
+}
+
+const inputPrice2 = (event: any) => {
+    console.log('event:', event.target.value);
+    Item2.price = event.target.value;
+}
 </script>
 
 <style scoped>
@@ -36,7 +84,7 @@ const url1 = 'https://www.amazon.co.jp/Jiandi-%E3%82%B2%E3%83%BC%E3%83%9F%E3%83%
     align-items: center;
     height: 80px;
     width: 400px;
-    background-color: blue;
+    background-color: rgb(160, 160, 231);
     margin-bottom: 8px;
 }
 
