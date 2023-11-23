caa1dc28bfe0ca1842ad190d7b273bb1533c8651
NatsukiHayashi <su811sh1@gmail.com>
2023-11-23 09:32:05 +0900

    ✨ チャプター38

--- src/components/Payment.vue
+++ src/components/Payment.vue
@@ -1,10 +1,15 @@
 <template>
     <div class="container">
         <h1>Payment</h1>
-        <input v-on:input="inputName1" />
-        <input v-on:input="inputPrice1" />
-        <input v-on:input="inputName2" />
-        <input v-on:input="inputPrice2" />
+        <input v-model="Item1.name" />
+        <!-- <input v-on:input="inputName1" v-bind:value="Item1.name" /> -->
+        <input v-model="Item1.price" />
+        <!-- <input v-on:input="inputPrice1" v-bind:value="Item1.price" /> -->
+        <input v-model="Item2.name" />
+        <!-- <input v-on:input="inputName2" v-bind:value="Item2.name" /> -->
+        <input v-model="Item2.price" />
+        <!-- <input v-on:input="inputPrice2" v-bind:value="Item2.price" /> -->
+        <button v-on:click="clear">Clear</button>
         <div class="payment">
             <label>{{ Item1.name }}</label>
             <label>{{ Item1.price }} yen</label>
@@ -24,20 +29,20 @@
 import { ref, reactive } from 'vue'
 
 const Item1 = reactive({
-    name: null,
-    price: null,
+    name: '',
+    price: 0,
 });
 
 const Item2 = reactive({
-    name: null,
-    price: null,
+    name: '',
+    price: 0,
 });
 
 // const ItemName1 = ref<string>('Desk');
-const ItemName2 = 'Bike';
+// const ItemName2 = 'Bike';
 
 // const ItemPrice1 = '40000';
-const ItemPrice2 = '20000';
+// const ItemPrice2 = '20000';
 
 const url1 = 'https://www.amazon.co.jp/Jiandi-%E3%82%B2%E3%83%BC%E3%83%9F%E3%83%B3%E3%82%B0%E3%83%87%E3%82%B9%E3%82%AF-%E3%83%91%E3%82%BD%E3%82%B3%E3%83%B3%E3%83%87%E3%82%B9%E3%82%AF-%E3%83%A9%E3%83%83%E3%82%AF%E4%BB%98%E3%81%8D%E3%83%87%E3%82%B9%E3%82%AF-%E3%82%AA%E3%83%95%E3%82%A3%E3%82%B9%E3%83%87%E3%82%B9%E3%82%AF/dp/B0B3Y9KXCS/ref=sr_1_5?__mk_ja_JP=%E3%82%AB%E3%82%BF%E3%82%AB%E3%83%8A&crid=9XSA60HQD2J2&keywords=desk&qid=1700574894&sprefix=desk,aps,172&sr=8-5&th=1'
 
@@ -68,6 +73,13 @@ const inputName2 = (event: any) => {
 const inputPrice2 = (event: any) => {
     console.log('event:', event.target.value);
     Item2.price = event.target.value;
+}
+
+const clear = () => {
+    Item1.name = '';
+    Item1.price = 0;
+    Item2.name = '';
+    Item2.price = 0;
 }
 </script>
 
